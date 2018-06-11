f7219329f0d164e9b045e8746e40ccf4cebf1f34
waluev <dima.vap2011@yandex.ru>
2018-06-11 20:06:42 +0300

    conf

--- Configuration.py
+++ Configuration.py
@@ -1,6 +1,6 @@
 TOKEN = '' #Ваш token
 MY_ID = '' #Ваш Id VK
 FRIEND_ID = '' #Id собеседника
-version_api = "5.78" #Версия API
+version_api = '5.78' #Версия API
 reverse = 1 # 1 - в хронологическо порядке, 0 - нет
 Friend_Name = '' #Так будет называться файл
